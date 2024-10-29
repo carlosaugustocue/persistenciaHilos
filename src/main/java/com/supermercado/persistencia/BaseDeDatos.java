56058f035e1db05f6968b49d36effacbe25fc8d0
Carlos Aranzazu <caaranzazu_230@cue.edu.co>
2024-10-28 22:26:43 -0500

    Modificacion en Basededatos.java

--- src/main/java/com/supermercado/persistencia/BaseDeDatos.java
+++ src/main/java/com/supermercado/persistencia/BaseDeDatos.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-// Asegúrate de importar las clases para manejar los recursos y propiedades
+
 import java.io.InputStream;
 import java.util.Properties;
 
